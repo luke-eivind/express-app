9c78a65ca257c4c93901e80ab3b87a4eb83282e6
Luke Freeman <lukefreeman@Lukes-MacBook-Pro.local>
2017-06-02 01:38:38 -0500

    added 404 and 500 error messages

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.get('/', function(req, res){
   res.render('home');
 });
 
+//defines route to 'about' page
 app.get('/about', function(req, res){
   res.render('about');
 });
@@ -42,6 +43,22 @@ app.use(function(err, req, res, next){
 app.get('/about', function(req, res){
   res.render('about');
 });
+
+app.use(function(req, res){
+  res.type('text/html');
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+})
+
+
+
+
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + ' press Ctrl-C to terminate');
