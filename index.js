07365487c321778a35a5618eb2dda5c469fc58e5
Luke Freeman <lukefreeman@Lukes-MacBook-Pro.local>
2017-06-02 01:24:50 -0500

    added routes

--- index.js
+++ index.js
@@ -2,8 +2,52 @@ var express = require('express');
 
 var app = express();
 
+app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//MORE IMPORTS HERE
+
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+//defines the root
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.use(function(req, res, next){
+  console.log('Looking for URL : ' + req.url);
+  next();
+})
+
+app.get('/junk', function(req, res, next){
+  console.log('Tried to access /junk');
+  throw new Error('/junk does\'nt exist');
+});
+
+app.use(function(err, req, res, next){
+  console.log('Error : ' + err.message);
+  next();
+});
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + ' press Ctrl-C to terminate');
+})
+
+/*
 app.get('/', function(req, res){
   res.send('Express Works');
 });
@@ -11,3 +55,4 @@ app.get('/', function(req, res){
 app.listen(app.get('port'), function(){
   console.log('Express started press Ctrl-C to terminate');
 });
+*/
